377fc0ca9af9af1f16b6452b1f773570f11d7451
Mikael <mikaelmendonca007@gamil.com>
2024-07-19 15:02:37 -0300

    Initial Commit

---
+++ UISwitch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISwitch
+//
+//  Created by Mikael Mendonca on 19/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
